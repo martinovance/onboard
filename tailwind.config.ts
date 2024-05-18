55de4afc528a9a03ddb42cbfc423a9658aa95b64
unknown <martinsogunsina@gmail.com>
2024-05-18 14:21:27 +0100

    UI Build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,10 @@ const config = {
     },
     extend: {
       colors: {
+        dark: {
+          1: '#1C1F2E',
+          2: '#151925',
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
