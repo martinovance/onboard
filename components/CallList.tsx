5be5a4c16e05e98b26771abb5e6a3c11615653b4
unknown <martinsogunsina@gmail.com>
2024-05-24 11:26:34 +0100

    App finalization

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,16 +1,21 @@
+// @ts-nocheck
+
 "use client"
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useGetCalls } from '@/hooks/useGetCalls';
 import { useRouter } from 'next/navigation';
 import { Call, CallRecording } from '@stream-io/video-react-sdk';
 import MeetingCard from '@/components/MeetingCard';
+import Loader from './Loader';
+import { useToast } from './ui/use-toast';
 
 const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
   const { endedCalls, upcomingCalls, callRecordings, isLoading } = useGetCalls();
   const [recordings, setRecordings] = useState<CallRecording[]>([])
   const router = useRouter();
+  const { toast } = useToast();
 
   const getCalls = () => {
     switch (type) {
@@ -38,8 +43,26 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
     }
   }
 
+  useEffect(() => {
+    try {
+      const fetchRecordings = async () => {
+        const callData = await Promise.all(callRecordings?.map((meeting) => meeting.queryRecordings()))
+  
+        const recordings = callData.filter(call => call.recordings.length > 0).flatMap(call => call.recordings)
+  
+        setRecordings(recordings);
+      }
+  
+      if (type === 'recordings') fetchRecordings();  
+    } catch (error) {
+      toast({ title: 'Try again later'})
+    }
+  }, [type, callRecordings])
+
   const calls = getCalls();
   const noCallsMessage = getNoCallsMessage();
+
+  if (isLoading) return <Loader />
 
   return (
     <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
@@ -56,7 +79,7 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
           title={
             (meeting as Call).state?.custom?.description ||
             (meeting as CallRecording).filename?.substring(0, 20) ||
-            'No Description'
+            'Personal Meeting'
           }
           date={
             (meeting as Call).state?.startsAt?.toLocaleString() ||
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -11,6 +11,7 @@ import { useToast } from "@/components/ui/use-toast"
 import MeetingModal from '@/components/MeetingModal'
 import { Textarea } from './ui/textarea'
 import ReactDatePicker from 'react-datepicker'
+import { Input } from './ui/input'
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -108,7 +109,7 @@ const MeetingTypeList = () => {
               Add a description
             </label>
             <Textarea 
-              className="border-none bg-dark-3 focus:visible:ring-0 focus:visible:ring-offset-0" 
+              className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0" 
               onChange={(e) => {setValues({...values, description: e.target.value}) }}
             />
           </div>
@@ -152,6 +153,21 @@ const MeetingTypeList = () => {
         buttonText="Start-Meeting"
         handleClick={createMeeting}
       />
+
+      <MeetingModal 
+        isOpen={meetingState === 'isJoiningMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Paste link here"
+        className="text-center"
+        buttonText="Join Meeting"
+        handleClick={() => router.push(values.link)}
+      >
+        <Input 
+          placeholder="Meeting link"
+          className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+          onChange={(e) => setValues({ ...values, link: e.target.value })}
+        />
+      </MeetingModal>
     </section>
   )
 }
